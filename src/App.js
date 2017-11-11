28b8bcd1c311cb5020bc142da5828a1501aaaf42
Jie Zhou <jie.z.zhou@gmail.com>
2017-11-11 13:11:47 -0500

    ejected, added scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="scroll-container">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
